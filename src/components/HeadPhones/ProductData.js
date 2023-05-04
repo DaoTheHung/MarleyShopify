abd7a296dc73a1d769d4f6a95e76439925ce8c8a
DaoTheHung <Daohung447@gmail.com>
2023-05-04 23:06:43 +0700

    task product sort

--- src/components/HeadPhones/ProductData.js
+++ src/components/HeadPhones/ProductData.js
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import Link from 'next/link';
 import Pagination from '../common/Pagination/Pagination';
 import { FormSelect } from '../common/Form/FormSelect';
 import { productSelector, getProducts } from '../../../store';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 const ProductData = () => {
     const [animate, setAnimate] = useState("left-0")
+
     // Get data redux
     const dispath = useDispatch()
+
     const products = useSelector(productSelector)
+    const [data, setData] = useState([])
 
     useEffect(() => {
-        dispath(getProducts())
-    }, [dispath])
-
+        fetch('https://api-test-two-beta.vercel.app/api/v1')
+            .then((res) => {
+                return (res.json())
+            }).then((data) => {
+                setData(data.data)
+            });
+        // const res = axios.get('https://api-test-two-beta.vercel.app/api/v1')
+        // .then()
+    }, [])
     // Pagination
 
     const [crPage, setCrPage] = useState(1)
@@ -24,7 +33,7 @@ const ProductData = () => {
     const lastCart = crPage * cartPage;
     const firtCart = lastCart - cartPage;
     const fakeProductPage = [...products]
-    const currenCart = fakeProductPage.slice(firtCart, lastCart)
+    const currenCart = data?.slice(firtCart, lastCart)
 
     const paginate = (pageNumber) => {
         setCrPage(pageNumber)
@@ -40,8 +49,8 @@ const ProductData = () => {
         <>
             <div className='w-[845px] ml-[50px]'>
 
-                <Row className='bg-[#0000000d] py-[16px] px-[6px]'>
-                    <Col xs>
+                <div className='bg-[#0000000d] py-[16px] px-[18px] flex justify-between'>
+                    <div >
                         <div className='flex relative w-[87px]'>
                             <div className={`text-[22px] text-white transition-allLinear duration-[500ms] absolute  bg-pink-500 w-[50%] h-full ${animate}`}>
                                 <i class="fa-sharp fa-solid fa-list p-[10px] border "></i>
@@ -53,17 +62,17 @@ const ProductData = () => {
                                 <i class="fa-sharp fa-solid fa-list p-[10px] border border-[#e9e9e9]"></i>
                             </div>
                         </div>
-                    </Col>
-                    <Col xs={{ order: 12 }} className='flex items-center'>
+                    </div>
+                    <div className='flex items-center'>
 
                         <h3 className='text-ink-100 text-[22px] font-normal'>Paginate by</h3>
-                        <FormSelect />
 
-                    </Col>
-                    <Col xs={{ order: 1 }} className='flex items-center'>
+                    </div>
+                    <div className='flex items-center gap-[5px]'>
                         <h3 className='text-ink-100 text-[22px] font-normal'>Sort by</h3>
-                    </Col>
-                </Row>
+                        <FormSelect setData={setData} data={data} />
+                    </div>
+                </div>
 
                 <div className={`${animate == "right-0" ? "flex flex-col gap-0" : "flex "} flex-wrap gap-[27px] mt-[24px] relative`}>
                     {currenCart?.map((product) => (
@@ -75,7 +84,7 @@ const ProductData = () => {
                                     <img className='transition   duration-[600ms] absolute top-0 opacity-[0] group-hover/edit:opacity-100' src={product.imageSlick1} />
                                 </div>
                                 <div className='transition-opacity duration-[600ms] group-hover/edit:bottom-[20px] w-full absolute bottom-1 opacity-[0]  group-hover/edit:opacity-100'>
-                                    <div className='pl-[115px]  text-[15px] tracking-[37px] gap-[10px]  flex'>
+                                    <div className='pl-[58px]  text-[15px] tracking-[37px] gap-[10px]  flex'>
                                         <Link style={{ textDecoration: "none" }} href={`/products/${product.name}`}>
                                             <div className='w-[45px] group h-[45px] transition duration-[500ms] rounded-[50%] hover:bg-pink-500 flex justify-center items-center pl-[37px] pt-[3px]'>
                                                 <i className="text-ink-300 fa-solid fa-link transition duration-[500ms] group-hover:text-[#fff]"></i>
@@ -90,7 +99,7 @@ const ProductData = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className={`${animate == "right-0" ? "flex flex-col gap-[20px] mt-[138px] ml-[22px]" : "text-center"}`}>
+                            <div className={`${animate == "right-0" ? "flex flex-col gap-[20px] mt-[138px] ml-[22px]" : "text-center "}`}>
                                 <div className={`${animate == "right-0" ? "" : "text-center"} tracking-[3px]`}>
                                     <i className="fa-solid text-[#1a1a1a6b] fa-star text-[12px]"></i>
                                     <i className="fa-solid text-[#1a1a1a6b] fa-star text-[12px]"></i>
--- src/components/common/Form/FormSelect.js
+++ src/components/common/Form/FormSelect.js
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { productSelector, getProducts } from '../../../../store'
+export const FormSelect = ({ setData }) => {
+    const [show, setShow] = useState(false)
+    const [type, setType] = useState()
 
-export const FormSelect = () => {
+    // Data select
+    const dataSort = [
+        {
+            name: "Featured"
+        },
+        {
+            name: "Best selling"
+        },
+        {
+            name: "Alphabetically, A-Z"
+        },
+        {
+            name: "Alphabetically, Z-A"
+        },
+        {
+            name: "Price, low to high"
+        },
+        {
+            name: "Price, high to low"
+        },
+        {
+            name: "Date, old to new"
+        },
+        {
+            name: "Date, new to old"
+        },
+    ]
+    // Get data redux
+    const products = useSelector(productSelector)
+    const dispath = useDispatch()
+
+    useEffect(() => {
+        dispath(getProducts())
+    }, [dispath])
+
+
+    // Handle data
+    const handleSortProduct = (name) => {
+        setType(name)
+        const arrProduct = [...products]
+
+        // Price, low to high
+        if (name === "Price, low to high") {
+            const lowPrice = arrProduct.sort((a, b) => a.price - b.price)
+            setData(lowPrice)
+        }
+
+
+        // Price, high to low
+        if (name === "Price, high to low") {
+            const highPrice = arrProduct.sort((a, b) => b.price - a.price)
+            setData(highPrice)
+        }
+
+    }
     return (
-        <div>
-
-        </div>
+        <>
+            <div onClick={() => setShow(!show)} className='w-[223px] cursor-pointer  h-[30px] bg-pink-500 px-[10px] flex items-center justify-between relative'>
+                <div className='text-[18px] text-white'>Featured</div>
+                <div className='mt-[-6px] text-white text-[12px]'><i class="fa-solid fa-sort-down"></i></div>
+                {show && <div className=' w-[223px] h-[218px] bg-[#fff] absolute top-[35px] left-0 z-10 overflow-y-scroll '>
+                    {dataSort.map((item, i) => (
+                        <div
+                            onClick={() => handleSortProduct(item.name)}
+                            style={type === item.name ? { background: "#3498db", color: "#fff" } : {}}
+                            className='pl-[13px] text-[18px] hover:bg-[#3498db] hover:text-[#fff] cursor-pointer leading-[35px] text-ink-100' key={i}>{item.name}</div>
+                    ))}
+                </div>}
+            </div>
+        </>
     )
 }
 
