5af113df1b426ffc2f08cf2ed0ba94ff1372dde9
Hùng <Daohung447@gmail.com>
2024-06-17 11:29:00 +0700

    fix: fix height product

--- src/components/HeadPhones/ProductData.js
+++ src/components/HeadPhones/ProductData.js
@@ -91,7 +91,7 @@ const ProductData = () => {
             <div className='xl:w-[1240px] md:w-[676px] ml-0 md:ml-[50px]'>
 
                 <div className='bg-[#0000000d] py-[16px] px-[18px] flex justify-between md:w-full  w-[343px] m-auto flex-col md:flex-row'>
-                    <div className='hidden lg:flex justify-center'>
+                    {/* <div className='hidden lg:flex justify-center'>
                         <div className='flex relative w-[87px] ml-[58px] md:ml-auto'>
                             <div className={`transition-allLinear duration-[500ms] absolute bg-pink-500 w-[50%] h-[99%] ${animate}`}>
 
@@ -111,9 +111,9 @@ const ProductData = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                     <div className='flex items-center flex-col md:flex-row '>
-                        <label className='text-ink-100 text-[22px] font-normal'>Search <span><i className="text-[15px] fa-solid fa-magnifying-glass"></i></span></label>
+                        <label className='text-ink-100 text-[22px] font-normal'>Search <span><i class="text-[15px] fa-solid fa-magnifying-glass"></i></span></label>
                         <div>
                             <input
 
@@ -132,9 +132,9 @@ const ProductData = () => {
                     {!isLoading ?
                         currenCart?.map((product) => (
 
-                            <div key={product.id} className={`${animate == "translate-x-[44px] w-full" ? "flex flex-row md:w-[31%]" : ""}  w-full  group/item overflow-hidden`}>
+                            <div key={product.id} className='boder relative rounded-[4px] pb-4  w-full lg:w-[245px] lg:h-[471px] flex flex-col justify-between group/item overflow-hidden items-center'>
                                 <div className='relative group/edit transition duration-[600ms] cursor-pointer group-hover/item:shadow-cart  group/edit '>
-                                    <div className={`${animate == "translate-x-[44px] w-[358px]" ? "w-full " : ""} relative`}>
+                                    <div className={`${animate == "translate-x-[44px]" ? "w-[358px] " : ""} relative`}>
                                         <img className='' src={product.image} />
                                         <img className='transition   duration-[600ms] absolute top-0 opacity-[0] group-hover/edit:opacity-100' src={product.imageSlick1} />
                                     </div>
@@ -154,7 +154,7 @@ const ProductData = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className={`${animate == "translate-x-[44px]" ? "flex flex-col gap-[20px] items-center" : "text-center "}`}>
+                                <div className={`${animate == "translate-x-[44px]" ? "flex flex-col gap-[20px] mt-[138px] ml-[22px]" : "text-center "}`}>
                                     <div className={`${animate == "translate-x-[44px]" ? "" : "text-center"} tracking-[3px]`}>
                                         <i className="fa-solid text-[#1a1a1a6b] fa-star text-[12px]"></i>
                                         <i className="fa-solid text-[#1a1a1a6b] fa-star text-[12px]"></i>
